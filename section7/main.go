816ed3440cc44e13aa1c843e7df088b7f246619a
Taisei <taisei96.web@gmail.com>
2023-11-21 08:50:29 +0900

    エラーハンドリングDone

--- section7/main.go
+++ section7/main.go
@@ -2,28 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 
 func main() {
 
 	// 条件分岐
-	a := 1
-	if a == 2 {
-		fmt.Println("two")
-	} else if a == 1 {
-		fmt.Println("one")
-	} else {
-		fmt.Println("I dont know")
-	}
+	// a := 1
+	// if a == 2 {
+	// 	fmt.Println("two")
+	// } else if a == 1 {
+	// 	fmt.Println("one")
+	// } else {
+	// 	fmt.Println("I dont know")
+	// }
 
-	if b := 100; b == 100 {
-		fmt.Println("one hundred")
-	}
+	// if b := 100; b == 100 {
+	// 	fmt.Println("one hundred")
+	// }
 
-	x := 0
-	if x:= 2; true {
-		fmt.Println(x)
+	// x := 0
+	// if x:= 2; true {
+	// 	fmt.Println(x)
+	// }
+	// fmt.Println(x)
+
+	// エラーハンドリング
+	var s string = "A"
+	
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println(err)
 	}
-	fmt.Println(x)
+	fmt.Printf("i = %T\n", i)
+
 }
